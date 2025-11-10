b303d430b39c19df598477a2cd4974d12fc93f0a
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-11-10 10:18:39 -0300

    tela de cadastro abrindo e fechando

---
+++ js/contatos.js
@@ -0,0 +1,68 @@
+export async function lerContatos() {
+    const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
+
+    const response = await fetch (url)
+
+    const contatos = await response.json()
+
+    return contatos
+}
+
+async function criarContato(contato) {
+    const url = 'https://bakcend-fecaf-render.onrender.com/contatos/'
+
+    const options = {
+        method:"POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(contato)
+    }
+
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+async function deletarContato (id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+
+    const options = {
+        method: 'DELETE'
+    }
+
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+async function atualizarContato(id, contato) {
+
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+
+    const options = {
+        method:"PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(contato)
+    }
+
+    const response = await fetch(url, options)
+
+
+    return response.ok
+    
+}
+
+const novoContato = {
+    "nome": "Guilherme Moreira",
+    "celular": "11 9 4002-8922",
+    "foto": "semFoto.png",
+    "email": "guilherme.moreira@gmail.com",
+    "endereco": "Av. Estrada bela, 333",
+    "cidade": "Jandira"
+}
+
+criarContato()
+// lerContatos()
--- js/main.js
+++ js/main.js
@@ -1,2 +1,66 @@
 'use strict'
 
+import { lerContatos } from "./contatos.js"
+
+// console.log(await lerContatos())
+
+function criarContatos (contato) {
+const container = document.getElementById('container')
+
+const divCardContato = document.createElement('div')
+const imagemContato = document.createElement('img')
+const nomeContato = document.createElement('h2')
+const numeroContato = document.createElement('p')
+
+divCardContato.classList.add('card-contato')
+
+imagemContato.src = contato.foto
+imagemContato.onerror = () => {
+    imagemContato.src = './img/avatar1.avif'
+}
+
+if (contato.nome) {
+    nomeContato.textContent = contato.nome
+  } else {
+    nomeContato.textContent = 'Nome desconhecido'
+  }
+
+if (contato.celular) {
+    numeroContato.textContent = contato.celular
+  } else {
+    numeroContato.textContent = 'Numero desconhecido'
+  }
+  
+
+container.appendChild(divCardContato)
+divCardContato.append(imagemContato, nomeContato, numeroContato)
+
+return container
+}
+
+async function carregarContatos() {
+    const contatos = await lerContatos()
+    contatos.forEach(criarContatos)
+}
+
+const buttonNovoContato = document.getElementById('novo-contato')
+
+function abrirTelaCadastro() {
+    const main = document.querySelector('main')
+
+    main.classList.remove('card-show')
+    main.classList.add('form-show')
+
+    document.getElementById('cancelar').addEventListener('click', fecharTelacadastro)
+
+}
+buttonNovoContato.addEventListener('click', abrirTelaCadastro)
+
+function fecharTelacadastro() {
+    const main = document.querySelector('main')
+
+    main.classList.remove('form-show')
+    main.classList.add('card-show')
+}
+
+carregarContatos()
